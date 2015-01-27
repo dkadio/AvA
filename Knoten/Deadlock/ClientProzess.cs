387ff392839a6d5581fa3afd789143b3dc0a98b3
dkadio <d.kadio@gmail.com>
2015-01-27 14:16:57 +0100

    Uebung 3 angefangen

---
+++ Knoten/Deadlock/ClientProzess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadlock
+{
+    class ClientProzess
+    {
+
+        String fileA;
+        String fileB;
+
+        public ClientProzess(String fileA, String fileB)
+        {
+            //starte immer mit fileA 
+        }
+
+        public void schreibRechtAnfordern(String datei)
+        {
+
+        }
+
+        public void inkrementFile(String datei)
+        {
+
+        }
+
+        public void dekrementFile(String datei)
+        {
+
+        }
+
+        public void changeFileName(String datei)
+        {
+
+        }
+    }
+}
---
+++ Knoten/Deadlock/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace Deadlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pid = Process.GetCurrentProcess().Id % 3;
+            if (pid == 0)
+            {
+                //starte mit File B
+                Console.WriteLine("B");
+                ClientProzess cp = new ClientProzess("b", "a");
+            }
+            else
+            {
+                //starte mit File A
+                Console.WriteLine("A");
+                ClientProzess cp = new ClientProzess("a", "b");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
